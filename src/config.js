8e0aed6b6dceda718fa8fa4829a30ddc8329d4dd
Arish <webdeveloper2@amvionlabs.com>
2025-02-28 11:01:39 +0530

    arish

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // config.js
-export const backendPort = 90;
+export const backendPort = 80;
 export const baseURL = `${window.location.protocol}//${window.location.hostname}:${backendPort}/sales/`;
